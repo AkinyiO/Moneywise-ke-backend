23de29a01c472e202bcaf368c2866e270b002b9c
Esther Camilla Akinyi <camillaodhiambo8@gmail.com>
2022-09-19 23:58:01 +0300

    Updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 puts "🌱 Seeding spices..."
 
 # Seed your database here
-Expense.create(name:"Shopping", cost: 1003) #, payment_id: 1)
-Expense.create(name:"Holiday", cost: 456) #, payment_id: 1)
-Expense.create(name:"Transportation", cost: 358) #, payment_id: 1)
-Expense.create(name:"Fuel", cost: 789) #payment_id: 1)
-Expense.create(name:"Child Care", cost: 1230) #, payment_id: 1)
+Expense.create(name:"Shopping", cost: 1003, payment_id: 1)
+Expense.create(name:"Holiday", cost: 456, payment_id: 2)
+Expense.create(name:"Transportation", cost: 358, payment_id: 3)
+Expense.create(name:"Fuel", cost: 789,payment_id: 4)
+Expense.create(name:"Child Care", cost: 1230, payment_id: 5)
 
 
 Payment.create(budget: 4590, amount_remaining: 904, spent_so_far: 2231)
