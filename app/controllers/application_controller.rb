91f6d415a86e8010be9e28f05233073b0e0d9d0e
Esther Camilla Akinyi <camillaodhiambo8@gmail.com>
2022-09-19 20:01:37 +0300

    Updated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,42 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Bigger luck with your project!" }.to_json
+  # get "/" do
+  #   { message: "Bigger luck with your project!" }.to_json
+  # end
+
+  get "/payments" do 
+    payments = Payment.all.order(:created_at)
+    payments.to_json
   end
-  
+
+  patch "/payments/:id" do 
+    payment = Payment.find(params[:id])
+    payment.update(budget:params[:budget])
+    payment.to_json
+    end
+
+    get "/expenses" do 
+      expenses = Expense.all.order(:created_at)
+      expenses.to_json
+    end
+
+    post "/expenses" do 
+      expense = Expense.create(name: params[:name], cost: params[:cost],payment_id: params[:payment_id])
+    expense.to_json
+    end  
+
+    patch "/expenses/:id" do 
+      expense =  Expense.find(params[:id])
+      expense.update(name: params[:name],cost: params[:cost])
+      expense.to_json
+    end
+
+    delete "/expenses/:id" do 
+      expense = Expense.find(params[:id])
+      expense.delete
+      expense.to_json
+    end
+
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,13 @@
 puts "🌱 Seeding spices..."
 
 # Seed your database here
-Expense.create(name:"Shopping", cost: 1003, payment_id: 1)
-Expense.create(name:"Holiday", cost: 456, payment_id: 1)
-Expense.create(name:"Transportation", cost: 358, payment_id: 1)
-Expense.create(name:"Fuel", cost: 789, payment_id: 1)
-Expense.create(name:"Child Care", cost: 1230, payment_id: 1)
+Expense.create(name:"Shopping", cost: 1003) #, payment_id: 1)
+Expense.create(name:"Holiday", cost: 456) #, payment_id: 1)
+Expense.create(name:"Transportation", cost: 358) #, payment_id: 1)
+Expense.create(name:"Fuel", cost: 789) #payment_id: 1)
+Expense.create(name:"Child Care", cost: 1230) #, payment_id: 1)
 
 
-Payment.create(budget: 4590, amount_remaining: 904, spent_so_far: 1)
+Payment.create(budget: 4590, amount_remaining: 904, spent_so_far: 2231)
 
 puts "✅ Done seeding!"
